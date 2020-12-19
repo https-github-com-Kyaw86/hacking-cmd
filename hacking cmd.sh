e49b2f3b1329726b697e9d0744e7d0d7b75e9cc2
Termux <70668507+TermuxHackz@users.noreply.github.com>
2020-12-19 14:08:38 +0100

    Add files via upload

---
+++ hacking cmd.sh
@@ -0,0 +1,85 @@
+#!/bin/bash                     
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by AnonyminHack5, now start"
+echo "---------------------------------------------------------------------------------"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo  "#####################################"
+echo   " we are anynomous"               
+ echo   " we are hackers "                                                                    
+echo   "####################################"
+
+echo "007"
+echo "007"
+echo "007"
+
+echo "."
+
+apt-get update -y                      #update in progress
+apt install python -y           #installation......
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install toilet -y
+apt install figlet -y
+echo  "#####################################"
+echo   " You can now hack anything "               
+ echo    " completing wait in 3 2 1 "                                                                    
+echo   "####################################"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+pkg install openssh -y
+pkg install hydra -y
+echo  "#####################################"
+echo   "Anonymous "               
+ echo                                                                      echo   "####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+
+echo " "                                                                   
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install fish -y
+apt install dnsutils -y
+                                                            echo   " "
+                                                            
+apt install unzip -y
+apt install tor -y 
+apt install wget -y
+apt install w3m -y
+apt install cmatrix -y
+apt install coreutils -y
+echo  "#####################################"
+echo   " we are hackers "               
+                                                                  echo   "####################################"
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+
+echo  "#####################################"
+               
+echo   " processing wait......
+"                                                                    
+echo   "####################################"
+
+echo " If You Connected To Internet Then u downloaded successfully subscribe to our youtube channel Gamer links ...we are learners dont underestimate us "
+
+echo " ??? Now rock and hack ??? "
+
+exit
